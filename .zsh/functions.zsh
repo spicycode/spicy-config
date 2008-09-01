4555804601a31e5ca5282375bfbeb62105474589
Chad Humphries <chad@spicycode.com>
2008-09-01 12:37:14 -0400

    Minor cleanup

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,11 +1,9 @@
 function update_textmate_bundles {
-  local bundles_directory='~/Library/Application\ Support/TextMate/Bundles'
-for dir in ~/Library/Application\ Support/TextMate/Bundles/*.tmbundle
-do
-  cd $dir
-  git pull
-  svn up
-  cd ~
-done
+  for dir in ~/Library/Application\ Support/TextMate/Bundles/*.tmbundle; do
+    cd $dir
+    git pull
+    svn up
+    cd ~
+  done
 }
 
