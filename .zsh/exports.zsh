4577f68f21e3ee03f88b8a9ec147253e0fbb3e29
Chad Humphries <chad@spicycode.com>
2008-11-11 10:55:11 -0500

    CDPATH Reorg

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,13 +1,11 @@
 export MANPATH=/opt/local/share/man:$MANPATH
 export PATH=/usr/local/git/bin:/Library/Ruby/bin:/opt/local/bin:/opt/local/lib/postgresql82/bin/:/opt/local/sbin:/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/:/usr/local/bin:$PATH
-export CDPATH=~/ruby:~/relevance
+export CDPATH=~/spicycode:~/open_source:~/relevance
 export RUBYOPT='rubygems'
 export GREP_OPTIONS='--color=auto' 
 export GREP_COLOR='3;33'
-#export EDITOR="mvim --remote-tab-wait"
 export EDITOR="vim"
 export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
-
 export TERM=xterm-256color
 export LSCOLORS=gxfxcxdxbxegedabagacad
 export CLICOLOR=1
