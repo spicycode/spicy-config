362ba891db0175461c766c08b557805314c94797
Chad Humphries <chad@spicycode.com>
2008-11-27 01:18:26 -0500

    With inspiration from topfunky, better RPS1 that indicats you need to checkin

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -2,12 +2,26 @@ parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\/git:\1/'
 }
 
-#parse_git_pair() {
-#  if [[ parse_git_branch == "" ]]; then
-#    git config --get user.name
-#  fi
-#}
- 
+# Show character if changes are pending
+git_status() {
+  if current_git_status=$(git status 2> /dev/null | grep 'added to commit' 2> /dev/null); then
+    echo "⚡"
+  else
+    echo ''
+  fi
+}
+
+git_prompt_info() {
+  branch_prompt=$(parse_git_branch)
+  if [ -n "$branch_prompt" ]; then
+  
+    
+    echo "$branch_prompt"
+    # echo "$fg[green]$branch_prompt$reset_color $(git_status)"
+  fi
+}
+
+    
 # Put the string "hostname::/full/directory/path" in the title bar:
 set_term_title() { 
 	echo -ne "\e]2;$PWD\a" 
@@ -35,4 +49,5 @@ postexec() {
   set_running_app
 }
 
-export PS1='%{$reset_color$fg[gray]%}%2~%{$reset_color$bold_color$fg[green]%}%{$reset_color$fg[green]%}$(parse_git_branch)>%{$reset_color%} ' 
+export PS1='%{$reset_color$fg[gray]%}%2~%{$reset_color$bold_color$fg[green]%}$(git_prompt_info)>%{$reset_color%} ' 
+export RPS1='%{$fg[yellow]%}$(git_status)%{$reset_color%}'
