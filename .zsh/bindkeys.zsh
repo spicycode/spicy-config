804fca45baae1ecb3be9a6fbd3739b99c7169ad4
Chad Humphries <chad@spicycode.com>
2009-03-10 22:58:19 -0400

    Reorganized bindkeys, added documentation, and made option-left, option-right go back/forward word

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -1,11 +1,12 @@
-# Reasonable defaults
-bindkey '^K' kill-whole-line
-bindkey "^R" history-incremental-search-backward
-bindkey "^A" beginning-of-line
-bindkey "^E" end-of-line
-bindkey "[B": history-search-forward
-bindkey "[A": history-search-backward 
-# In case of trouble, smash, err, Enable VIM keybindings
-# bindkey -v
-# bindkey -rM vicmd "/"
-# bindkey ^R history-incremental-search-backward
+# To see the key combo you want to use just do:
+# cat > /dev/null
+# And press it
+
+bindkey '^K' kill-whole-line                      # ctrl-k
+bindkey "^R" history-incremental-search-backward  # ctrl-r
+bindkey "^A" beginning-of-line                    # ctrl-a  
+bindkey "^E" end-of-line                          # ctrl-e
+bindkey "[B": history-search-forward              # down arrow
+bindkey "[A": history-search-backward             # up arrow
+bindkey '^[[D' backward-word                      # option left
+bindkey '^[[C' forward-word                       # option right
