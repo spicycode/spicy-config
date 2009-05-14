9f14c3eab037c3d0978b13ea1742343340d210d9
Chad Humphries <chad@spicycode.com>
2009-05-14 18:09:38 -0400

    Yes, I'm bashing bash

--- .bash.d/history.sh
+++
@@ -1,5 +0,0 @@
-export HISTFILESIZE=1000
-#  ignore duplicate commands, commands that begin with a space, and the ‘exit’ command
-export HISTIGNORE="&:[ ]*:exit"
-# Append history
-shopt -s histappend
