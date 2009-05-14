68430fd98b27f25943b7e713d33b0a22760aac89
Chad Humphries <chad@spicycode.com>
2009-05-14 16:42:06 -0400

    Documentation

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,16 +1,26 @@
+# Currently this path is appendend to dynamically when picking a ruby version
+export ORIGINAL_PATH=/opt/local/bin:/opt/local/sbin:/usr/local/bin:/opt/local/lib/postgresql83/bin/:$PATH
+
+# Make the jruby home directory available as a constant for later use
+export JRUBY_HOME='~/Projects/opensource/jruby-1.2.0'
+
+# Our list of directorys we can cd to from anywhere
+export CDPATH=.:~/Projects/spicycode:~/Projects/open_source:~/Projects/relevance
+
 # Add macports man pages to the man path
 export MANPATH=/opt/local/share/man:$MANPATH
 
-export ORIGINAL_PATH=/opt/local/bin:/opt/local/sbin:/usr/local/bin:/opt/local/lib/postgresql83/bin/:$PATH
+# Set default console Java to 1.6
 export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home'
-export JRUBY_HOME='~/Projects/opensource/jruby-1.2.0'
-export CDPATH=.:~/Projects/spicycode:~/Projects/open_source:~/Projects/relevance
-export GREP_OPTIONS='--color=auto' 
-export GREP_COLOR='3;33'
-export EDITOR="mvim -f"
+
+# Setup terminal, and turn on colors
 export TERM=xterm-color
 export LSCOLORS=gxfxcxdxbxegedabagacad
 export CLICOLOR=1
+
+# Enable color in grep
+export GREP_OPTIONS='--color=auto' 
+export GREP_COLOR='3;33'
 
 # Enable color, and better prompt in LESS
 export PAGER=less
@@ -18,3 +28,6 @@ export LESS=ce3M
 
 # This resolves issues install the mysql, postgres, and other gems with native non universal binary extensions
 export ARCHFLAGS='-arch i386'
+
+# Set macvim with flag to wait for console (git commit compat) as our EDITOR
+export EDITOR="mvim -f"
