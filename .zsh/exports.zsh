fc7f8f43d2a52404474c122093e3b566de5d3647
Chad Humphries <chad@spicycode.com>
2009-05-14 10:21:12 -0400

    unsetting RUBYOPT, and switching git prompt for vcs_info.
      - Note, on OSX this requires zsh-devel from MacPorts, or the latest
        ZSH manually compiled

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -3,7 +3,6 @@ export ORIGINAL_PATH=/opt/local/bin:/opt/local/sbin:/usr/local/bin:/opt/local/li
 export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home'
 export JRUBY_HOME='~/Projects/opensource/jruby-1.2.0'
 export CDPATH=.:~/Projects/spicycode:~/Projects/open_source:~/Projects/relevance
-export RUBYOPT='rubygems'
 export GREP_OPTIONS='--color=auto' 
 export GREP_COLOR='3;33'
 export EDITOR="mvim -f"
--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,9 +1,12 @@
-git_prompt_info() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null)
-  if [[ -n $ref ]]; then
-    echo "%{$fg_bold[green]%}/${ref#refs/heads/}%{$reset_color%}"
-  fi
-}
+autoload -Uz vcs_info && vcs_info
+RED=$'%{\e[31m%}'
+GR=$'%{\e[32m%}'
+MA=$'%{\e[35m%}'
+YE=$'%{\e[33m%}'
+NC=$'%{\e[0m%}'
+zstyle ':vcs_info:*' actionformats  "/${GR}%s:${YE}${GR}%b${YE}|${RED}%a${NC}"
+zstyle ':vcs_info:*' formats        "/${GR}%s:${Y}${GR}%b%}${NC}"
+zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat "%b${RED}:${YE}%r"
 
 # Put the string "hostname::/full/directory/path" in the title bar:
 set_term_title() { 
@@ -23,6 +26,7 @@ precmd() {
   if [ $TERM_PROGRAM ]; then
     set_term_title
     set_term_tab
+    vcs_info
   fi
 }
 
@@ -36,4 +40,4 @@ postexec() {
   set_running_app
 }
 
-export PS1='%{$reset_color$fg[gray]%}%2~%{$reset_color$bold_color$fg[green]%}$(git_prompt_info)>%{$reset_color%} '
+export PS1='%{$reset_color$fg[gray]%}%2~%{$reset_color%}%{$vcs_info_msg_0_%}>%{$reset_color%} '
