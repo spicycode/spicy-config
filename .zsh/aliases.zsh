aaed671ff1223ed7041ca637223ade42144e9e22
Chad Humphries <chad@spicycode.com>
2009-05-14 12:06:02 -0400

    Updating aliases
      - Adding g for git
      - port now runs 'sudo port -v'
      - Adding ll
      - Made rdoc do exactly what I want

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,9 +1,17 @@
+# Aliases that do not change the command
+alias g='git'
 # My aliases
-alias ocaml="rlwrap ocaml"
+alias port='sudo port -v'
+alias ocaml='rlwrap ocaml'
+# Override macports ssh in case it gets pulled in as a dependency
 alias ssh='/usr/bin/ssh'
-alias ls='ls -G'
+# Colorize output, add file type indicator, and put sizes in human readable format
+alias ls='ls -GFh'
+# Same as above, but in long listing format
+alias ll='ls -GFhl'
 alias rmate='mate Rakefile README app bin config doc db lib public script spec test stories features examples'
 alias ri='ri -Tf ansi'
+alias rdoc='rdoc --ri --line-numbers --all'
 alias ss='./script/server'
 alias sc='./script/console'
 alias vi='/Applications/MacVim.app/Contents/MacOS/Vim -p'
