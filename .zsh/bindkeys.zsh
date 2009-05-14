53df139fbf5b7e6bc80d986d8c4920c86d022744
Chad Humphries <chad@spicycode.com>
2009-05-14 15:25:34 -0400

    Cleanup, remove word jump

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -2,14 +2,9 @@
 # cat > /dev/null
 # And press it
 
-bindkey '^K' kill-whole-line                      # ctrl-k
-bindkey "^R" history-incremental-search-backward  # ctrl-r
-bindkey "^A" beginning-of-line                    # ctrl-a  
-bindkey "^E" end-of-line                          # ctrl-e
-bindkey "[B": history-search-forward              # down arrow
-bindkey "[A": history-search-backward             # up arrow
-bindkey '\eOc' forward-word		# ctrl cursor right
-bindkey '\eOd' backward-word # ctrl cursor left
-
-# bindkey '^[[D' backward-word                      # option left
-# bindkey '^[[C' forward-word                       # option right
+bindkey "^K"      kill-whole-line                      # ctrl-k
+bindkey "^R"      history-incremental-search-backward  # ctrl-r
+bindkey "^A"      beginning-of-line                    # ctrl-a  
+bindkey "^E"      end-of-line                          # ctrl-e
+bindkey "[B"      history-search-forward               # down arrow
+bindkey "[A"      history-search-backward              # up arrow
