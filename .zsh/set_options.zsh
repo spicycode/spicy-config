63ed7e25cc3b43e419d25538a6d7f2b411289937
Chad Humphries <chad@spicycode.com>
2009-05-14 12:07:39 -0400

    Documentation, and additional setopt settings

--- .zsh/set_options.zsh
+++ .zsh/set_options.zsh
@@ -1,10 +1,46 @@
+# ===== Basics
+
+# If you type foo, and it isn't a command, and it is a directory in your cdpath, go there
+setopt AUTO_CD
+
+# Help us out with basic spelling correction for commands
+setopt CORRECT
+
+# Allow comments even in interactive shells (especially for Muness)
+setopt INTERACTIVE_COMMENTS
+
+# ===== History
+
+# Allow multiple terminal sessions to all append to one zsh command history
 setopt APPEND_HISTORY 
+
+# Add comamnds as they are typed, don't wait until shell exit
 setopt INC_APPEND_HISTORY 
+
+# Do not write events to history that are duplicates of previous events
 setopt HIST_IGNORE_DUPS
+
+# When searching history don't display results already cycled through twice
 setopt HIST_FIND_NO_DUPS
-setopt complete_in_word         # Not just at the end
-setopt always_to_end            # When complete from middle, move cursor
-setopt nohup										# In general, we don't kill background jobs upon logging out
-setopt prompt_subst
-setopt auto_cd
-setopt extendedglob
+
+# Remove extra blanks from each command line being added to history
+setopt HIST_REDUCE_BLANKS
+
+# Include more information about when the command was executed, etc
+setopt EXTENDED_HISTORY
+
+# Enables real-time sharing of history across terminal sessions
+setopt SHARE_HISTORY
+
+# ===== Completion 
+
+# Allow completion from within a word/phrase
+setopt COMPLETE_IN_WORD 
+
+# When completing from the middle of a word, move the cursor to the end of the word
+setopt ALWAYS_TO_END            
+
+# ===== Prompt
+
+# Enable parameter expansion, command substitution, and arithmetic expansion in the prompt
+setopt PROMPT_SUBST
