88caf9a6ec6f425234714d47d64b0225253d8de3
Chad Humphries <chad@spicycode.com>
2009-06-23 22:52:05 -0400

    Updating unpushed

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,4 +1,4 @@
-autoload -Uz vcs_info && vcs_info
+#autoload -Uz vcs_info && vcs_info
 RED=$'%{\e[31m%}'
 GR=$'%{\e[32m%}'
 MA=$'%{\e[35m%}'
@@ -25,7 +25,7 @@ function set_running_app {
 function precmd { 
   set_term_title
   set_term_tab
-  vcs_info
+  #vcs_info
 }
 
 function preexec { 
@@ -36,4 +36,33 @@ function postexec {
   set_running_app
 }
 
-export PS1='%{$reset_color$fg[gray]%}%2~%{$reset_color%}%{$vcs_info_msg_0_%}>%{$reset_color%} '
+function parse_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\/git:\1/'
+}
+
+function git_prompt_info() {
+  local ref=$(git symbolic-ref HEAD 2> /dev/null)
+  local gitst="$(git status 2> /dev/null)"
+
+  if [[ -f .git/MERGE_HEAD ]]; then
+    if [[ ${gitst} =~ "unmerged" ]]; then
+      gitstatus=" %{$fg[red]%}unmerged%{$reset_color%}"
+    else
+      gitstatus=" %{$fg[green]%}merged%{$reset_color%}"
+    fi
+  elif [[ ${gitst} =~ "Changes to be committed" ]]; then
+    gitstatus=" %{$fg[green]%}modified%{$reset_color%}"
+  elif [[ ${gitst} =~ "use \"git add" ]]; then
+    gitstatus=" %{$fg[red]%}modified%{$reset_color%}"
+  elif [[ -n `git checkout HEAD 2> /dev/null | grep ahead` ]]; then
+    gitstatus=" %{$fg[yellow]%}unpushed ↑ %{$reset_color%}"
+  else
+    gitstatus=''
+  fi
+  if [[ -n $ref ]]; then
+    echo "%{$fg_bold[green]%}/${ref#refs/heads/}%{$reset_color%}$gitstatus"
+  fi
+}
+
+export PS1='%{$reset_color$fg[gray]%}%2~%{$reset_color$bold_color$fg[green]%}$(git_prompt_info)>%{$reset_color%} '
+#export PS1='%{$reset_color$fg[gray]%}%2~%{$reset_color%}%{$vcs_info_msg_0_%}>%{$reset_color%} '
