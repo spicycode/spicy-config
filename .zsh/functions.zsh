986c457eb27ac1ce8aef87418337eb91ef2bc4d9
Chad Humphries <chad@spicycode.com>
2009-09-07 14:19:04 -0400

    Adding in functions from http://tomafro.net

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -32,6 +32,29 @@ function mysqlredo {
 function nginx_start { sudo nginx }
 
 function nginx_stop { sudo kill $(cat /opt/nginx/logs/nginx.pid) }
- 
+
+# From http://tomafro.net/tags/zsh
+
 # Change directory and open TextMate in a single command
 function tm { cd $1; rmate $1 }
+
+# Usage is simple: tab <command> opens a new tab in Terminal, and runs the given command in the current working directory. 
+# For example tab script/server would open a new tab and run script/server.
+function tab {
+  osascript 2>/dev/null <<EOF
+    tell application "System Events"
+      tell process "Terminal" to keystroke "t" using command down
+    end
+    tell application "Terminal"
+      activate
+      do script with command "cd $PWD; $*" in window 1
+    end tell
+EOF
+}
+
+# Create and move to a directory in a single command
+# Usage: take ~/Projects/tools/awesometer
+function take {
+  mkdir -p $1
+  cd $1
+}
